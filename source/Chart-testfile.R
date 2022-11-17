7155d5bd7037412dee0db0fbc7cf8268f547e5ed
S-Singh1699 <singh718@uw.edu>
2022-11-17 15:51:06 -0800

    Pushing updated file

--- source/Chart-testfile.R
+++ source/Chart-testfile.R
@@ -11,63 +11,25 @@ myPalette <- brewer.pal(5, "Set2")
 
 ss_df <- read.csv(file = "C:\\Users\\simra\\OneDrive\\Documents\\INFO201\\project-group-6-section-ae\\data\\School Shootings Dataset - INFO 201\\pah_wikp_combo.csv")
 
-# basic histogram
-
-total_c_shootings <- ss_df %>%
-  filter(School == "C")
-
-num_c_shootings <- nrow(total_c_shootings)
-
-total_hs_shootings <- ss_df %>%
-  filter(School == "HS")
-
-num_hs_shootings <- nrow(total_hs_shootings)
-
-total_ms_shootings <- ss_df %>%
-  filter(School == "MS")
-
-num_ms_shootings <- nrow(total_ms_shootings)
-
-total_es_shootings <- ss_df %>%
-  filter(School == "ES")
-
-num_es_shootings <- nrow(total_es_shootings)
-
-vec_total_shootings <- c(num_c_shootings, num_hs_shootings, num_ms_shootings, num_es_shootings)
-
-vec_school_type <-c("C", "HS", "MS", "ES")
-
-testing_data <- group_by(ss_df, School)
-
-count_type <- ss_df %>%
-  group_by(School) %>%
-  count()
-
 #Table grouping by one feature - State
 state_table <- ss_df %>%
   group_by(State) %>%
-  count(name = "Number of Shootings") %>%
+  count(name = "Number of Shootings")
   
-
-
 # Barplot
     
-    #NEW
-ggplot(ss_df, aes(fill=AreaType, y=num_es_shootings, x=ss_df$School)) + 
-  geom_bar(position="dodge", stat="identity")
+shooting_count_table <- ss_df %>%
+  group_by(School) %>%
+  count(name = "Number_of_Shootings") %>%
+  filter(School != "")
 
-  #DIFFERENT
-ggplot(ss_df, aes(x=ss_df$School, y=num_es_shootings)) + 
-  geom_bar(stat = "identity")
+  shooting_count_table$School <- recode(shooting_count_table$School, "-" = "Unknown Type")
 
-#OLD
-total_shootings <- ss_df %>%
-  filter(School == "HS")
+#Shooting_Table Bar Graph
 
-hs_shootings <- nrow(total_shootings)
-
-ggplot(ss_df, aes(x=ss_df$School, y=nrow(hs_shootings))) + 
-  geom_bar(stat = "identity")
+ggplot(shooting_count_table, aes(x=School, y=Number_of_Shootings)) + 
+  geom_bar(stat = "identity", color="black", fill = "red") +
+  labs(x = "School", y = "Number of Shootings", title = "Shootings Based on School Type")
 
 #PieChart
   urban <- ss_df %>%
@@ -87,12 +49,13 @@ ggplot(ss_df, aes(x=ss_df$School, y=nrow(hs_shootings))) +
  
   vec_areatype <- c(num_urban, num_suburban, num_rural)
   
-
-
-pie(vec_areatype , labels = c("Urban","Suburban","Rural"), border="black", col=myPalette, main = "Shootings Based On Area" )
+pie(vec_areatype , labels = c("Urban","Suburban","Rural"),  main = "Shootings Based On Area",  border="black", col=myPalette)
 
 
 #LinePlot
+
+#Redo format properly for date
+#Group by function for new tbale of just years and fatalities
 
 deaths_plot <- ggplot(data = ss_df) + 
   geom_line(mapping = aes(x = as.Date(Date), y = Sum(Fatalities))) +
