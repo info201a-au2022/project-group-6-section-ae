ff035c19773dae3d1a8d4e10339fed3c95fbd740
Ashwin Subramanian <ashwinsubr@Ashwins-MacBook-Pro-2.local>
2022-11-17 18:32:02 -0800

    pushing summary info file

--- source/summary_info.R
+++ source/summary_info.R
@@ -1,25 +1,49 @@
 #summary_info.R
-ss_df <- read.csv(file = "C:\\Users\\simra\\OneDrive\\Documents\\INFO201\\project-group-6-section-ae\\data\\School Shootings Dataset - INFO 201\\pah_wikp_combo.csv")
+library(dplyr)
+ss_df <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-6-section-ae/main/data/School%20Shootings%20Dataset%20-%20INFO%20201/pah_wikp_combo.csv")
 
 # A source file that takes in a dataset and returns a list of info about it:
 summary_info <- list()
 
+# Number of incidents in the data frame
 summary_info$num_observations <- nrow(ss_df)
 
-summary_info$some_max_value <- my_dataframe %>%
-  filter(some_var == max(some_var, na.rm = T)) %>%
-  select(some_label)
-
+#Area type with the most number of shootings
 area_stats <- ss_df %>%
   group_by(AreaType) %>%
   count() %>%
-  filter(n == max(n)) %>%
+  filter(AreaType != "") %>%
+  arrange(-n) %>%
+  head(1) %>%
   pull(AreaType)
 
+summary_info$area_stats <- area_stats
+
+#Type of school with the most number of shootings
 type_of_school <- ss_df %>%
   group_by(School) %>%
   count() %>%
-  filter(n == max(n)) %>%
+  filter(School != "") %>%
+  filter(School != "-") %>%
+  arrange(-n) %>%
+  head(1) %>%
   pull(School)
 
+summary_info$type_of_school <- type_of_school
 
+#State with the most number of shootings
+state_with_highest <- ss_df %>%
+  group_by(State) %>%
+  count() %>%
+  arrange(-n) %>%
+  head(1) %>%
+  pull(State)
+
+summary_info$state_with_highest <- state_with_highest
+
+#Highest fatalaties in a single shooting
+highest_fatalities <- ss_df %>%
+  filter(Fatalities == max(Fatalities)) %>%
+  pull(Fatalities)
+
+summary_info$highest_fatalities <- highest_fatalities
